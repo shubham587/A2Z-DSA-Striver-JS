c8ff8cea6493d5f84eec45d252fd89c1184c41b4
Shubham Pawar <shubhampwr9945@gmail.com>
2024-05-28 21:58:53 +0530

    recursion probs

--- Recursion/recursionProb.js
+++ Recursion/recursionProb.js
@@ -8,7 +8,7 @@ function printName(i, n){
     if(i == n)return
     printName(i+1, n)
 }
-
+// -----------------------------------
 // Print 1 to N using Recursion
 
 // printNumber(1, n)
@@ -21,6 +21,21 @@ function printNumber(i, n){
         return
     } 
 }
+// -----------------------------------
+
+// Print N to 1 using Recursion
+
+// printRevNumber(1, n)
+
+function printRevNumber(i, n){
+    if(0 !== n){
+        console.log(n);
+        printRevNumber(i, n-1)
+    }else{
+        return
+    } 
+}
+// -----------------------------------
 
 // backtracking
 
@@ -36,6 +51,7 @@ const backTrack = (i, n) => {
 
 n =5
 // backTrack(1, n)
+// -----------------------------------
 
 // sum till n
 
@@ -46,4 +62,43 @@ const sumBack = (i, n) => {
 
 
 n=5
-console.log(sumBack(1, n))
+// console.log(sumBack(1, n))
+// -----------------------------------
+
+// Factorial of a Number : Iterative and Recursive
+n = 3
+// Functional approach recursion
+const factFunctional = (i, n) => {
+    if(i > n)return 1
+    return i * factFunctional(i+1, n)
+}
+
+// console.log(factFunctional(1, n));
+
+// parameterized approach recursion
+
+const factPara = (n, fact=1) => {
+    if(n == 0){
+        console.log(fact);
+        return
+    }
+    factPara(n-1, fact*n)
+}
+
+// factPara(5)
+// -----------------------------------
+
+// Reverse a given Array
+let arr = [1,2,3,4,5]
+
+const revArr = (i=0) => {
+    if(arr.length < i)return
+    let n = arr.length
+    temp = arr[n-i]
+    arr[n-i] = arr[i]
+    arr[i] = temp
+    revArr(i+1)
+}
+
+revArr(arr)
+console.log(arr, "arr");
